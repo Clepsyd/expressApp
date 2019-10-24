4b4cc0a30d8ea5d4dfee91caa05324ee9a2fe371
Guillaume <liard.guillaume@gmail.com>
2019-10-24 12:34:58 +0100

    added css, image is a constatn size

--- public/scripts/potatoSwitch.js
+++ public/scripts/potatoSwitch.js
@@ -5,10 +5,10 @@ window.onload = function() {
   this.createCoolPotato = function () {
     const normalPotato = document.getElementById("normalPotato");
 
-    const coolPotato = document.createElement('IMG')
-    coolPotato.id = "coolPotato"
-    coolPotato.src = 'images/coolPotato.jpg'
-    coolPotato.addEventListener("click", createNormalPotato)
+    const coolPotato = document.createElement('IMG');
+    coolPotato.id = "coolPotato";
+    coolPotato.src = 'images/coolPotato.jpg';
+    coolPotato.addEventListener("click", createNormalPotato);
 
     potatoDiv.removeChild(normalPotato);
     potatoDiv.appendChild(coolPotato);
@@ -17,10 +17,10 @@ window.onload = function() {
   this.createNormalPotato = function () {
     const coolPotato = document.getElementById("coolPotato");
 
-    const normalPotato = document.createElement('IMG')
-    normalPotato.id = "normalPotato"
-    normalPotato.src = 'images/potato.png'
-    normalPotato.addEventListener("click", createCoolPotato)
+    const normalPotato = document.createElement('IMG');
+    normalPotato.id = "normalPotato";
+    normalPotato.src = 'images/potato.png';
+    normalPotato.addEventListener("click", createCoolPotato);
 
     potatoDiv.removeChild(coolPotato);
     potatoDiv.appendChild(normalPotato);
