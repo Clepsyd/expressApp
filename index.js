c9f1b1d08f8e8c24f7fb84fcc8e479e34892e0ba
Guillaume <liard.guillaume@gmail.com>
2019-10-24 13:42:24 +0100

    changed port settings

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-   console.log("Request: ", req.method, req.url);
-   console.log("Response: ", res.statusCode)
+  res.sendFile(path.join(__dirname + 'index.html'));
+  console.log("Request: ", req.method, req.url);
+  console.log("Response: ", res.statusCode)
 });
 
-const server = app.listen(3000, function () {
-   var port = server.address().port
-   
+const server = app.listen(port, function () {   
    console.log("Example app listening at port %s", port)
 });
