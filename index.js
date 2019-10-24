83f1755473eb2989232c73b4e40468c4c0cd4d58
Guillaume <liard.guillaume@gmail.com>
2019-10-24 20:49:09 +0100

    added new route: takes name as query string

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
+const viewsFolder = path.join(__dirname + '/views')
+
 app.use(express.static('public'));
+app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + 'index.html'));
+  res.sendFile(`${viewsFolder}/index.html`);
   console.log("Request: ", req.method, req.url);
   console.log("Response: ", res.statusCode)
 });
+
+app.get('/name-your-potato', function (req, res) {
+  res.render('name-your-potato.pug', {
+    name: req.query['name'].toLowerCase()
+  });
+})
 
 const server = app.listen(port, function () {   
    console.log("Example app listening at port %s", port)
