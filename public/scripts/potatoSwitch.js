5a67388c304f781450b1b2df5e04237ada62ab26
Guillaume <liard.guillaume@gmail.com>
2019-10-24 13:15:39 +0100

    Text changes too

--- public/scripts/potatoSwitch.js
+++ public/scripts/potatoSwitch.js
@@ -1,29 +1,42 @@
 window.onload = function() {
 
   const potatoDiv = document.getElementById("potatoDiv");
+  const potatoText = document.getElementById("potatoText");
 
-  this.createCoolPotato = function () {
-    const normalPotato = document.getElementById("normalPotato");
-
+  const createCoolPotato = function () {
     const coolPotato = document.createElement('IMG');
     coolPotato.id = "coolPotato";
     coolPotato.src = 'images/coolPotato.jpg';
-    coolPotato.addEventListener("click", createNormalPotato);
-
-    potatoDiv.removeChild(normalPotato);
-    potatoDiv.appendChild(coolPotato);
+    coolPotato.addEventListener("click", changePotato);
+    return coolPotato
   }
 
-  this.createNormalPotato = function () {
-    const coolPotato = document.getElementById("coolPotato");
-
+  const createNormalPotato = function () {
     const normalPotato = document.createElement('IMG');
     normalPotato.id = "normalPotato";
     normalPotato.src = 'images/potato.png';
-    normalPotato.addEventListener("click", createCoolPotato);
-
-    potatoDiv.removeChild(coolPotato);
-    potatoDiv.appendChild(normalPotato);
+    normalPotato.addEventListener("click", changePotato);
+    return normalPotato
   }
 
+  function changePicture() {
+    let potatoPic = potatoDiv.getElementsByTagName("img")[0];
+    potatoDiv.removeChild(potatoPic);
+    potatoPic.id == "normalPotato" ?
+      potatoDiv.appendChild(createCoolPotato()) :
+      potatoDiv.appendChild(createNormalPotato());
+  }
+
+  function changeText() {
+    potatoText.innerHTML != "Swag potato here, click me if you can't handle it..." ?
+      potatoText.innerHTML = "Swag potato here, click me if you can't handle it..." :
+      potatoText.innerHTML = "Oooh... I'm boring again... Click me ?";
+  }
+
+  function changePotato () {
+    changePicture();
+    changeText();
+  }
+
+  globalThis.changePotato = changePotato;
 }
